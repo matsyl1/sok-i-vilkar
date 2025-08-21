3dad0432a95f1485d3ef97644b87f3fa646edf57
matsyl1 <156823556+matsyl1@users.noreply.github.com>
2025-08-21 19:08:26 +0200

    chore(backend): config build and frontend serving

---
+++ backend/dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const app = (0, express_1.default)();
+const PORT = 3000;
+// serve statisk fil fra front
+app.use(express_1.default.static(path_1.default.join(__dirname, 'dist')));
+app.get('/', (req, res) => {
+    res.send('hi from back');
+});
+app.listen(PORT, () => {
+    console.log('Server running on http://localhost:${PORT}');
+});
+//# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const PORT = 3000;
+
+// serve statisk fil fra front
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
     res.send('hi from back')
