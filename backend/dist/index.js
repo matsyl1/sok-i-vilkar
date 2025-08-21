7ed32b3371cfbbe21075be2223bffc01ac385793
matsyl1 <156823556+matsyl1@users.noreply.github.com>
2025-08-21 19:56:53 +0200

    fix(backend): fresh build after prev changes

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -8,10 +8,14 @@ const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 const PORT = 3000;
 // serve statisk fil fra front
-app.use(express_1.default.static(path_1.default.join(__dirname, 'dist')));
-app.get('/', (req, res) => {
-    res.send('hi from back');
+app.use(express_1.default.static(path_1.default.join(__dirname, '../public')));
+// spa fallback
+app.get('*', (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, '../public/index.html'));
 });
+// app.get('/', (req, res) => {
+//     res.send('hi from back')
+// });
 app.listen(PORT, () => {
     console.log('Server running on http://localhost:${PORT}');
 });
