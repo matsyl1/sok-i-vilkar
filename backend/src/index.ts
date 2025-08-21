9f53433428ea07934b116d8507c06b3ec035d1e8
matsyl1 <156823556+matsyl1@users.noreply.github.com>
2025-08-21 19:41:47 +0200

    fix(backend): serve React front and add SPA fallback

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,9 +7,14 @@ const PORT = 3000;
 // serve statisk fil fra front
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/', (req, res) => {
-    res.send('hi from back')
-});
+// spa fallback
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+})
+
+// app.get('/', (req, res) => {
+//     res.send('hi from back')
+// });
 
 app.listen(PORT, () => {
     console.log('Server running on http://localhost:${PORT}')
