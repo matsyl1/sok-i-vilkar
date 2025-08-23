482d16855e4d7ad8ce23ce36f5a9c9535df7be85
matsyl1 <156823556+matsyl1@users.noreply.github.com>
2025-08-23 20:48:33 +0200

    chore(backend): configure ESLint and fix errors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,14 +8,14 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '../public')));
 
 // spa fallback
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-})
+app.get('/*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 // app.get('/', (req, res) => {
 //     res.send('hi from back')
 // });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`Server running on http://localhost:${PORT}`);
 });
