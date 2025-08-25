5adb3a309d4d6b0a4558301af3f3072591a2622c
matsyl1 <156823556+matsyl1@users.noreply.github.com>
2025-08-25 17:36:43 +0200

    chore(backend): refactor index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,22 @@
-import express from 'express';
-import path from 'path';
+const express = require('express');
+const path = require('path');
+const searchRouter = require('./server')
+
+import type { Request, Response } from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//serve søkruter fra server.ts
+app.use('/api', searchRouter);
 
 // serve statisk fil fra front
 app.use(express.static(path.join(__dirname, '../public')));
 
 // spa fallback
-app.get('/*', (_req, res) => {
+app.get('/*', (_req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
-
-// app.get('/', (req, res) => {
-//     res.send('hi from back')
-// });
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
