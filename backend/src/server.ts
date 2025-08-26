58e0d1c742f44b79c3723a46b91d649f88a8492f
matsyl1 <156823556+matsyl1@users.noreply.github.com>
2025-08-26 18:36:41 +0200

    feat(backend): adjust /search endpoint to return JSON

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,8 +4,14 @@ import type { Request, Response } from 'express';
 
 const router = express.Router();
 
-router.get('/search', async (_req: Request, res: Response) => {
-  res.status(200).json({ status: 'OK', message: 'search route working' });
+router.get('/search', async (req: Request, res: Response) => {
+  // res.status(200).json({ status: 'OK', message: 'search route working' });
+  const query = req.query.q as string;
+  if (!query) {
+    return res.status(400).json({ status: 'ERROR', message: 'missing query' });
+  }
+
+  return res.status(200).json({ status: 'OK', message: `query recieved by backend: ${query}` });
 });
 
 module.exports = router;
