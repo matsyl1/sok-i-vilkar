8b6e59dbfcd5606ed03ee93cbba868e622c87129
matsyl1 <156823556+matsyl1@users.noreply.github.com>
2025-08-27 18:42:46 +0200

    feat(backend): add pdfParser in utils.ts and expand /search endpoint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,26 @@
 const express = require('express');
+const pdfParser = require('./utils');
 
 import type { Request, Response } from 'express';
 
 const router = express.Router();
 
 router.get('/search', async (req: Request, res: Response) => {
-  // res.status(200).json({ status: 'OK', message: 'search route working' });
   const query = req.query.q as string;
+
   if (!query) {
     return res.status(400).json({ status: 'ERROR', message: 'missing query' });
   }
 
-  return res.status(200).json({ status: 'OK', message: `query recieved by backend: ${query}` });
+  const result = await pdfParser(query);
+  return res.status(200).json({
+    status: 'OK',
+    document: result.filename,
+    query: query,
+    nrOfMatches: result.count,
+    matches: result.matches,
+  });
+
 });
 
 module.exports = router;
