5904f7bf84746047b97266fa2e2c3ba0f47e1791
matsyl1 <156823556+matsyl1@users.noreply.github.com>
2025-08-31 17:53:47 +0200

    chore(deploy): new build for render

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,20 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require('express');
-const pdfParser = require('./utils');
+const { parsePdfs, findPdfs } = require('./utils');
 const router = express.Router();
 router.get('/search', async (req, res) => {
     const query = req.query.q;
     if (!query) {
         return res.status(400).json({ status: 'ERROR', message: 'missing query' });
     }
-    const result = await pdfParser(query);
+    const pdfs = findPdfs();
+    const result = await parsePdfs(query, pdfs);
     return res.status(200).json({
         status: 'OK',
-        document: result.filename,
         query: query,
-        nrOfMatches: result.count,
-        matches: result.matches,
+        document: result,
     });
 });
 module.exports = router;
